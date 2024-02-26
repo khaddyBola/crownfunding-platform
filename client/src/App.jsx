975f7fda828a2f185ae5e3f294760d28fa2d8b11
khaddyBola <khadijahmusa232@gmail.com>
2024-02-26 12:30:45 +0100

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex-row'>
+      <div className='sm:flex hidden mr-10 relative'>Sidebar</div>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>Navbar</div>
+    </div>
   )
 }
 
